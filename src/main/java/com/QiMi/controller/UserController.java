33a8cb934180e698ddecfc4445735c42109534db
yuanhaosen <2816373096@qq.com>
2022-09-22 10:46:26 +0800

    1

--- src/main/java/com/QiMi/controller/UserController.java
+++ src/main/java/com/QiMi/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.QiMi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/test")
 @RestController
 public class UserController {
 
+    @GetMapping("/zzz")
+    public String zzz(){
+        return "hallo world";
+    }
 
 }
