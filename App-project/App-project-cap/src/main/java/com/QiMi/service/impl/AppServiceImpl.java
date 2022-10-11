971982e4f2802c950be4e28c961d5da547ba348a
yuanhaosen <2816373096@qq.com>
2022-10-11 16:02:44 +0800

    框架

---
+++ App-project/App-project-cap/src/main/java/com/QiMi/service/impl/AppServiceImpl.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+
+package com.QiMi.service.impl;
+
+import com.QiMi.service.AppService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author lengleng
+ * @date 2018/11/14
+ * <p>
+ * 手机登录相关业务实现
+ */
+@Slf4j
+@Service
+@AllArgsConstructor
+public class AppServiceImpl implements AppService {
+
+
+}
